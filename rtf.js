8fbddd2e368217bce49af8787da3322b7e1384ee
chuckmonkey <115195104+chuckmonkey@users.noreply.github.com>
2023-05-10 10:42:26 -0700

    Create rtf.js

---
+++ rtf.js
@@ -0,0 +1,31 @@
+function toRTF(alt_text) {
+  // 12 apr 2023 [cm] .. take alt text and convert to rtf
+  var rtfPrefix=
+          "{\\rtf1\\ansi\\ansicpg1252\\deff0"+
+            "\\deflang1033"+
+            "{\\fonttbl"+
+              "{\\f0\\fnil\\fcharset0 Microsoft Sans Serif;}"+
+              "{\\f1\\fmodern\\fprq6\\fcharset134 SimSun;}"+
+            "}";
+  
+  var rtfDelim="\\"+"'"+"";
+  //rtfDelim="";
+  
+  var rtfText="";
+  if ((alt_text===null) || (alt_text.length<=0) ) {
+    return rtfText;
+  }
+  var iIndex=0;
+  for (iIndex=0; iIndex<alt_text.length; iIndex++) {
+    var charValue=alt_text.charCodeAt(iIndex);
+    
+    rtfText+="\\u"+charValue+rtfDelim;
+  }
+  
+  rtfText=rtfPrefix+rtfText+"}";
+  //rtfText=rtfText.replaceAll("\'","&#39;");
+  return rtfText;
+  
+  //setElementValue("comment_rtf", rtfText);
+  //navigator.clipboard.writeText(rtfText);
+}
